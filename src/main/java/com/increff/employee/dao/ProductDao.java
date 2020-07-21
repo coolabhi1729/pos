934453d77c29a59e2daa43c3be0bc6365c6a5904
Abhi <abhikr.chs123@gmail.com>
2020-07-21 14:46:10 +0530

    Till Brand Part

--- src/main/java/com/increff/employee/dao/ProductDao.java
+++ src/main/java/com/increff/employee/dao/ProductDao.java
@@ -1,56 +1,56 @@
-package com.increff.employee.dao;
-
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-import javax.persistence.TypedQuery;
-import javax.transaction.Transactional;
-
-import org.springframework.stereotype.Repository;
-
-import com.increff.employee.pojo.BrandMasterPojo;
-import com.increff.employee.pojo.ProductPojo;
-
-@Repository
-public class ProductDao extends AbstractDao {
-	private static String delete_id = "delete from ProductPojo p where id=:id";
-	private static String select_id = "select p from ProductPojo p where id=:id";
-	private static String select_all = "select p from ProductPojo p";
-	private static String select_barcode = "select p from ProductPojo p where barcode=:barcode";
-	@PersistenceContext
-	private EntityManager em;
-
-	@Transactional
-	public void insert(ProductPojo p) {
-		em.persist(p);
-	}
-
-	public int delete(int id) {
-		Query query = em.createQuery(delete_id);
-		query.setParameter("id", id);
-		return query.executeUpdate();
-	}
-
-	public ProductPojo select(int id) {
-		TypedQuery<ProductPojo> query = getQuery(select_id, ProductPojo.class);
-		query.setParameter("id", id);
-		return getSingle(query);
-	}
-
-	public List<ProductPojo> selectAll() {
-		TypedQuery<ProductPojo> query = getQuery(select_all, ProductPojo.class);
-		return query.getResultList();
-	}
-
-//	public ProductPojo select(String name, String category) {
-//		TypedQuery<ProductPojo> query = getQuery(get_barcode, ProductPojo.class);
+//package com.increff.employee.dao;
 //
+//import java.util.List;
+//
+//import javax.persistence.EntityManager;
+//import javax.persistence.PersistenceContext;
+//import javax.persistence.Query;
+//import javax.persistence.TypedQuery;
+//import javax.transaction.Transactional;
+//
+//import org.springframework.stereotype.Repository;
+//
+//import com.increff.employee.pojo.BrandMasterPojo;
+//import com.increff.employee.pojo.ProductPojo;
+//
+//@Repository
+//public class ProductDao extends AbstractDao {
+//	private static String delete_id = "delete from ProductPojo p where id=:id";
+//	private static String select_id = "select p from ProductPojo p where id=:id";
+//	private static String select_all = "select p from ProductPojo p";
+//	private static String select_barcode = "select p from ProductPojo p where barcode=:barcode";
+//	@PersistenceContext
+//	private EntityManager em;
+//
+//	@Transactional
+//	public void insert(ProductPojo p) {
+//		em.persist(p);
+//	}
+//
+//	public int delete(int id) {
+//		Query query = em.createQuery(delete_id);
+//		query.setParameter("id", id);
+//		return query.executeUpdate();
+//	}
+//
+//	public ProductPojo select(int id) {
+//		TypedQuery<ProductPojo> query = getQuery(select_id, ProductPojo.class);
+//		query.setParameter("id", id);
 //		return getSingle(query);
 //	}
-
-	public void update(ProductPojo p) {
-
-	}
-}
+//
+//	public List<ProductPojo> selectAll() {
+//		TypedQuery<ProductPojo> query = getQuery(select_all, ProductPojo.class);
+//		return query.getResultList();
+//	}
+//
+////	public ProductPojo select(String name, String category) {
+////		TypedQuery<ProductPojo> query = getQuery(get_barcode, ProductPojo.class);
+////
+////		return getSingle(query);
+////	}
+//
+//	public void update(ProductPojo p) {
+//
+//	}
+//}
--- src/main/java/com/increff/employee/pojo/ProductPojo.java
+++ src/main/java/com/increff/employee/pojo/ProductPojo.java
@@ -1,57 +1,57 @@
-package com.increff.employee.pojo;
-
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
-
-@Entity
-@Table(name = "product", uniqueConstraints = { @UniqueConstraint(columnNames = { "barcode" }) })
-public class ProductPojo {
-
-	private int pid;
-	private String barcode;
-	private int brand_category;
-	private String product_name;
-	private double mrp;
-
-	public int getPid() {
-		return pid;
-	}
-
-	public void setPid(int pid) {
-		this.pid = pid;
-	}
-
-	public String getBarcode() {
-		return barcode;
-	}
-
-	public void setBarcode(String barcode) {
-		this.barcode = barcode;
-	}
-//cautious in Brand_Category
-	public int getBrand_category() {
-		return brand_category;
-	}
-
-	public void setBrand_category(int brand_category) {
-		this.brand_category = brand_category;
-	}
-
-	public String getProduct_name() {
-		return product_name;
-	}
-
-	public void setProduct_name(String product_name) {
-		this.product_name = product_name;
-	}
-
-	public double getMrp() {
-		return mrp;
-	}
-
-	public void setMrp(double mrp) {
-		this.mrp = mrp;
-	}
-
-}
+//package com.increff.employee.pojo;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.Table;
+//import javax.persistence.UniqueConstraint;
+//
+//@Entity
+//@Table(name = "product", uniqueConstraints = { @UniqueConstraint(columnNames = { "barcode" }) })
+//public class ProductPojo {
+//
+//	private int pid;
+//	private String barcode;
+//	private int brand_category;
+//	private String product_name;
+//	private double mrp;
+//
+//	public int getPid() {
+//		return pid;
+//	}
+//
+//	public void setPid(int pid) {
+//		this.pid = pid;
+//	}
+//
+//	public String getBarcode() {
+//		return barcode;
+//	}
+//
+//	public void setBarcode(String barcode) {
+//		this.barcode = barcode;
+//	}
+////cautious in Brand_Category
+//	public int getBrand_category() {
+//		return brand_category;
+//	}
+//
+//	public void setBrand_category(int brand_category) {
+//		this.brand_category = brand_category;
+//	}
+//
+//	public String getProduct_name() {
+//		return product_name;
+//	}
+//
+//	public void setProduct_name(String product_name) {
+//		this.product_name = product_name;
+//	}
+//
+//	public double getMrp() {
+//		return mrp;
+//	}
+//
+//	public void setMrp(double mrp) {
+//		this.mrp = mrp;
+//	}
+//
+//}
